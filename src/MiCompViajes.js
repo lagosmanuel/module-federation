35c0bad33f05091b965fe14f2a6715ad72936e0f
lagosmanuel <lagosmanual@gmail.com>
2024-09-23 19:50:16 -0300

    change component style

--- src/MiCompViajes.js
+++ src/MiCompViajes.js
@@ -3,7 +3,7 @@ import React from "react";
 function MiCompViajes() {
     // return a jsx ul list of a menu with five random items
     return <div>
-       <p> Descubren nueva isla flotante en el Pacífico que se mueve a 30 km/h: Científicos reportan el hallazgo de una isla flotante en movimiento que podría revolucionar el turismo. Los primeros viajeros ya se embarcan para conocer este destino único que cambia de ubicación constantemente. ¡Las reservas se agotaron en minutos!</p>
+       <p><strong>Descubren nueva isla flotante en el Pacífico que se mueve a 30 km/h</strong>: Científicos reportan el hallazgo de una isla flotante en movimiento que podría revolucionar el turismo. Los primeros viajeros ya se embarcan para conocer este destino único que cambia de ubicación constantemente. ¡Las reservas se agotaron en minutos!</p>
     </div>;
 }
 
